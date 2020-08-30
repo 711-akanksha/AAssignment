206ae6ff53b45d16eebc15312dd9e8eefac154ac
unknown <akanksha1116gupta@gmail.com>
2020-08-30 17:17:12 +0530

    assignment3

---
+++ assignment3/multiplicationoperator.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+/*Modify Overload the multiplication operator to enable multiplication of two complex numbers as in algebra. */
+class complex{
+public:
+complex(int r=0,int i=0)
+{
+real=r;
+img=i;
+}
+void print(){
+    cout<<real<<" +i"<<img<<endl;
+}
+friend complex operator *(const complex &,const complex& ); // Multiplication
+
+
+private:
+int real, img;
+};
+
+
+complex operator*(const complex &obj1,const complex &obj2)
+ {
+	 return complex(obj1.real * obj2.real, obj1.img * obj2.img);
+ }
+
+int main(){
+
+complex c1(12,7),c2(2,4);
+complex c3=c1*c2;
+c1.print();
+c2.print();
+c3.print();
+
+
+
+
+}
