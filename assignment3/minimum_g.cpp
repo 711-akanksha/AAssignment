bbfcb63d057ef2c1869ac02ae3ee957fb6a9134d
unknown <akanksha1116gupta@gmail.com>
2020-09-02 10:25:12 +0530

    assignment3

--- assignment3/minimum_g.cpp
+++ assignment3/minimum_g.cpp
@@ -1,26 +1,32 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
 using namespace std;
- int main()
+int recursiveMinimum(int array[],int start,int end)
 {
-int a[100],result;
-int n,i;
-cin>>n;
-for(i=0;i<n;i++)
-cin>>a[i];
-result = findMin(a[],0, n)
-cout<<"result"<<result;
-return 0;
-}
-int findMin(int a[], int start, int n)
-{
-
     int min;
-    if ( start ==n )
+    if(start==end)//stop condition for recursion
     {
-      return  min = a[start];
+        return array[start];//returning the array element at start (end) index
     }
-
-    // By convention, we assume ans[0] as max and ans[1] as min
-
+    //comparing element at start and end index
+    if(array[start]<array[end])//if element at start index is smaller one
+    {
+        min=recursiveMinimum(array,start,end-1);//than eliminate the element at last index
+    }
+    else//else (element at end index is smaller one)
+    {
+        min=recursiveMinimum(array,start+1,end);//than eliminate the element at start index
+    }
+    return min;//return the minimum value
 }
+int main()
+{
+    int array[10]={10,20,5,2,8,30,25,28,45,40};
+    cout<<"Array contains : "<<endl;
+    for(int i=0;i<10;i++)
+    {
+        cout<<array[i]<<" ";//printing the array elements
+    }
+    //testing the recursiveMinimum function by passing array and start index as 0 and end index as 9
+    cout<<endl<<"\nMinimum element in array is : "<<recursiveMinimum(array,0,9);
+}
+
